5743cbb55d35f523eab3e7efb9f57e51dc11ab29
Sree Ayyala <shanuayyala@outlook.com>
2019-05-06 20:57:37 -0500

    package update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Info from './components/Info';
@@ -8,17 +10,20 @@ import TodoListMain from './components/todolistComponents/TodoList_main';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">The Pomodoro Timer!</h1>
-        </header>
-        <div className="row container_main">
-          <div className="col-sm align-items-center"><Info/></div>
-          <div className="col-sm align-items-center"><Timer/></div>
-          <div className="col-sm align-items-center"><TodoListMain/></div>
-        </div>
-      </div>
+    return ( <
+      div className = "App" >
+      <
+      header className = "App-header" >
+      <
+      h1 className = "App-title" > The Pomodoro Timer! < /h1> <
+      /header> <
+      div className = "row container_main" >
+      <
+      div className = "col-sm align-items-center" > < Info / > < /div> <
+      div className = "col-sm align-items-center" > < Timer / > < /div> <
+      div className = "col-sm align-items-center" > < TodoListMain / > < /div> <
+      /div> <
+      /div>
     );
   }
 }
